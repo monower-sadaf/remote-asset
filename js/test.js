887398adab6483ae293b539a52ff3d645143d77c
Monower Sadaf <123621523+monower-sadaf@users.noreply.github.com>
2024-09-03 18:02:46 +0600

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,2 +1,4 @@
 const body = document.getElementsByTagName('body')[0];
+const blue = document.getElementByClassName('bg-blue-300');
+blue.style.backgroundColor = 'black';
 body.style.backgroundColor = 'red';
